e7708f735cf812da9672b648c9019556c2a2459f
smchahat <smchahat47@gmail.com>
2025-11-05 23:13:40 -0800

    leetcode 875

--- Regression/logistic_regression/python_basic/koko_monkey.py
+++ Regression/logistic_regression/python_basic/koko_monkey.py
@@ -5,11 +5,14 @@ h = 8
 
 m = max(piles)
 total = 0
-
+st = 1
 while True:
 
+    mid = (st + m)/2
     for i in piles:
         total += i
 
     if i > h:
-        
+        st = mid
+    elif i < h:
+        m = mid
